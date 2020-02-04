0ce4a7936793f4deb75df457ea04434a2d309589
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2020-02-04 12:41:31 +0300

    update the store file

--- ambrose-blog-1/src/store.js
+++ ambrose-blog-1/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux'
-import postReducer from './reducers'
+import rootReducer from './reducers'
 import thunk from 'redux-thunk'
 
-const store = createStore(postReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 export default store
