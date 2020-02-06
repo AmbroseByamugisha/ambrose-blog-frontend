2e52eab30c2fdcd67fb18e72e7d8ca875282257e
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2020-02-06 12:38:06 +0300

    update the post reducers

--- ambrose-blog-1/src/reducers/posts.js
+++ ambrose-blog-1/src/reducers/posts.js
@@ -1,22 +1,38 @@
-const initialState = [
-    {
+const initialState = {
+   posts: [ {
       id: 1, 
       title: 'Post One', 
-      body: "Sed dui sapien, rutrum nec laoreet in, ultricies condimentum arcu"
+      body: "Sed dui sapien, rutrum nec laoreet in, ultricies condimentum arcu",
+      deleted: false
     },
     {id: 2, 
       title: 'Post Two', 
-      body: 'Orci varius natoque penatibus et magnis dis parturient montes, '
+      body: 'Orci varius natoque penatibus et magnis dis parturient montes, ',
+      deleted: false
     },
-    {id: 3, title: 'Post Three', body: 'Post body three'}    
-]
+    {id: 3, title: 'Post Three', body: 'Post body three', deleted: false} 
+  ],
+  isdeleted: false   
+  }
 
 export default (state = initialState, action) => {
     switch(action.type) {
       case 'ADD_POST':
-        return state.concat([action.data])
+        return {
+          ...state,
+          posts:state.posts.concat([action.data])
+        }
       case 'DELETE_POST':
-        return state.filter((post)=>post.id !== action.id);
+        return {
+          ...state,
+          posts: state.posts.filter((post)=>post.id !== action.id),
+          isdeleted: true
+        } 
+      case 'TOGGLE_ISDELETED':
+        return {
+          ...state,
+          isdeleted: false
+        }
       case 'EDIT_POST':
         return state.map((post)=>post.id === action.id ? {...post,editing:!post.editing}:post)
       case 'UPDATE_POST':
