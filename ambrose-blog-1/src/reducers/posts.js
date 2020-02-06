911c93600396c23b10e2a8142c914556bb1df087
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2020-02-06 18:03:05 +0300

    update post reducer

--- ambrose-blog-1/src/reducers/posts.js
+++ ambrose-blog-1/src/reducers/posts.js
@@ -3,16 +3,25 @@ const initialState = {
       id: 1, 
       title: 'Post One', 
       body: "Sed dui sapien, rutrum nec laoreet in, ultricies condimentum arcu",
-      deleted: false
+      deleted: false,
+      editing: false
     },
     {id: 2, 
       title: 'Post Two', 
       body: 'Orci varius natoque penatibus et magnis dis parturient montes, ',
-      deleted: false
+      deleted: false,
+      editing: false
     },
-    {id: 3, title: 'Post Three', body: 'Post body three', deleted: false} 
+    {
+      id: 3, 
+      title: 'Post Three', 
+      body: 'Post body three', 
+      deleted: false,
+      editing: false
+  } 
   ],
-  isdeleted: false   
+  isdeleted: false,
+  isEditing: false   
   }
 
 export default (state = initialState, action) => {
@@ -34,18 +43,23 @@ export default (state = initialState, action) => {
           isdeleted: false
         }
       case 'EDIT_POST':
-        return state.map((post)=>post.id === action.id ? {...post,editing:!post.editing}:post)
+        return {
+          ...state,
+          posts: state.posts.map((post)=>post.id === action.id ? {...post,editing:!post.editing}:post),
+          isEditing: true
+        }
       case 'UPDATE_POST':
-        return state.map((post)=>{
-          if(post.id === action.id) {
-            return {
-               ...post,
-               title:action.data.newTitle,
-               body:action.data.newMessage,
-               editing: !post.editing
-            }
-          } else return post;
-        })
+        return {
+          ...state,
+          posts: state.posts.map((post)=> post.id === action.id ? 
+              {
+                 ...post,
+                 title:action.data.newTitle,
+                 body:action.data.newBody,
+                 editing: !post.editing
+              } : post),
+              isEditing:false
+        } 
       default:
         return state;
     }
